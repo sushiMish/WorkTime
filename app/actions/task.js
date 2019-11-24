ae1623fc7b50bde8be5db63fbf9480be43f8f948
sushiMish <sushila.mishra12@gmail.com>
2019-11-24 08:14:08 +0530

    Fix delete issue

--- app/actions/task.js
+++ app/actions/task.js
@@ -1,67 +1,63 @@
+import uuidv4 from 'uuid/v4';
+import moment from 'moment';
 import { saveSession } from './session';
 import db from '../db';
-const uuidv4 = require('uuid/v4')
-const moment = require('moment')
 
-export const FETCH_TASKS = 'FETCH_TASKS'
+export const FETCH_TASKS = 'FETCH_TASKS';
 
-export const getTasks = () => db.get('tasks').value()
+export const getTasks = () => db.get('tasks').value();
 
 const fetchTasks = () => {
   const tasks = getTasks();
   return {
     type: FETCH_TASKS,
     tasks
-  }
-}
+  };
+};
 
-export const saveTask = task => {
-  return (dispatch: Dispatch) => {
-    db.get('tasks')
-      .push({ ...task, id: uuidv4() })
-      .write()
+export const saveTask = task => (dispatch: Dispatch) => {
+  db.get('tasks')
+    .push({ ...task, id: uuidv4() })
+    .write();
 
-    dispatch(fetchTasks())
-  }
-}
+  dispatch(fetchTasks());
+};
 
 export const startTask = taskId => {
-  const startTime = moment().format()
+  const startTime = moment().format();
   return (dispatch: Dispatch) => {
     db.get('tasks')
       .find({ id: taskId })
       .assign({ startTime, endTime: null })
-      .write()
+      .write();
 
-    dispatch(fetchTasks())
-  }
-}
+    dispatch(fetchTasks());
+  };
+};
 
 export const stopTask = taskId => {
-  const endTime = moment().format()
+  const endTime = moment().format();
   return (dispatch: Dispatch) => {
     const { startTime, client, task, desc } = db
       .get('tasks')
       .find({ id: taskId })
-      .value()
+      .value();
 
     db.get('tasks')
       .find({ id: taskId })
       .assign({ startTime: undefined, endTime: undefined })
-      .write()
+      .write();
 
-    dispatch(saveSession({taskId, client, task, desc, startTime, endTime}));
+    dispatch(saveSession({ taskId, client, task, desc, startTime, endTime }));
 
-    dispatch(fetchTasks())
-  }
-}
+    dispatch(fetchTasks());
+  };
+};
 
-export const deleteTask = task => {
-  return (dispatch: Dispatch) => {
-    db.get('tasks')
-      .pop({ ...task, id: uuidv4() })
-      .write()
+export const deleteTask = id => (dispatch: Dispatch) => {
+  db.get('tasks')
+    .remove({ id })
+    .write();
 
-    dispatch(fetchTasks())
-  }
-}
+  dispatch(fetchTasks());
+};
